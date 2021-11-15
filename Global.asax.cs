6359baac35f0806e66864d9f9f10e3bccb7a476b
luisflores <luisflores@DESKTOP-ML04QFQ>
2021-11-15 12:05:45 -0500

    Agregar archivos de proyecto.

---
+++ Global.asax.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.Http;
+
+namespace WebApiSegura
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
